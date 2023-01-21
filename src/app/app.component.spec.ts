b0567fe7aa66bb6589bbd6ea6946a96a39a8b5b6
Harn1sh <41642384+Harn1sH@users.noreply.github.com>
2023-01-21 15:37:47 +0530

    .

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hotelinventory'`, () => {
+  it(`should have as title 'Inventory'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hotelinventory');
+    expect(app.title).toEqual('Inventory');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('hotelinventory app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Inventory app is running!');
   });
 });
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,18 +1,57 @@
-import { Component,OnInit } from '@angular/core';
-import { room } from './rooms'
-import { deets } from './rooms'
-import {SharedService} from '../shared.service'
+import { Component, OnInit } from '@angular/core';
+import {deets,room} from './rooms';
+
 @Component({
-  selector: 'hinv-rooms',
+  selector: 'app-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.scss']
 })
-export class RoomsComponent {
+export class RoomsComponent implements OnInit {
 
+  constructor() {
+    this.logvalue = "User";
+   }
+   roomdeets : deets[] = [];
+
+  ngOnInit(): void {
+
+    this.roomdeets= [
+      {
+        roomid: 1,
+        roomtype: 'Deluxe Room',
+        ameneties: 'AC and Mini Fridge',
+        price: 2000,
+        photos: 'https://images.pexels.com/photos/70441/pexels-photo-70441.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        checkintime: new Date('13-Jan-2023'),
+        checkouttime: new Date('14-Jan-2023'),
+      },
+      {
+        roomid: 2,
+        roomtype: 'Luxe Room',
+        ameneties: 'AC Mini bar and Mini Fridge',
+        price: 5000,
+        photos: 'https://images.pexels.com/photos/70441/pexels-photo-70441.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        checkintime: new Date('13-Jan-2023'),
+        checkouttime: new Date('14-Jan-2023'),
+      },
+      {
+        roomid: 3,
+        roomtype: 'Private Room',
+        ameneties: 'AC Mini bar and Mini Fridge',
+        price: 10000,
+        photos: 'https://images.pexels.com/photos/70441/pexels-photo-70441.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        checkintime: new Date('13-Jan-2023'),
+        checkouttime: new Date('14-Jan-2023'),
+      }
+    ]
+
+  }
+
+  
   loghide: boolean = false;
   logvalue!: string;
 
-  tl : boolean = false;
+  tl : boolean = false; 
 
   hotelname = 'Whaaaaaaaaat Whaaaaat';
   th = false;
@@ -33,35 +72,7 @@ export class RoomsComponent {
   dummy = "Hide Details";
   tv!:string;
 
-  roomdeets: deets[] = [
-    {
-      roomid: 1,
-      roomtype: 'Deluxe Room',
-      ameneties: 'AC and Mini Fridge',
-      price: 2000,
-      photos: 'https://images.pexels.com/photos/70441/pexels-photo-70441.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-      checkintime: new Date('13-Jan-2023'),
-      checkouttime: new Date('14-Jan-2023'),
-    },
-    {
-      roomid: 2,
-      roomtype: 'Luxe Room',
-      ameneties: 'AC Mini bar and Mini Fridge',
-      price: 5000,
-      photos: 'https://images.pexels.com/photos/70441/pexels-photo-70441.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-      checkintime: new Date('13-Jan-2023'),
-      checkouttime: new Date('14-Jan-2023'),
-    },
-    {
-      roomid: 3,
-      roomtype: 'Private Room',
-      ameneties: 'AC Mini bar and Mini Fridge',
-      price: 10000,
-      photos: 'https://images.pexels.com/photos/70441/pexels-photo-70441.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-      checkintime: new Date('13-Jan-2023'),
-      checkouttime: new Date('14-Jan-2023'),
-    }
-  ]
+  
 
   toggle() {
 
@@ -106,10 +117,7 @@ export class RoomsComponent {
 
   }
 
-  constructor( private lhide : SharedService){
 
-
-  }
 
   tothide(){
     
@@ -117,15 +125,4 @@ export class RoomsComponent {
     this.tv = this.logvalue;
   }
 
-  ngOnInit(){
-    this.loghide = this.lhide.logghide;
-    this.logvalue = this.lhide.loggvalue;
-    console.log("OnInit",this.loghide , this.tl);
-
-    this.tothide();
-    
-    
-  }
-
 }
-
